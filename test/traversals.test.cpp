8b5abc3602149122e766ee2f6a91ea5bd089731e
Joonas Lipping <lippinj@outlook.com>
2020-08-09 12:20:15 +0300

    Add tests for increment()

--- test/traversals.test.cpp
+++ test/traversals.test.cpp
@@ -1,4 +1,5 @@
 #include "grid.h"
+#include "operation.h"
 #include "pairwise.h"
 
 #include <catch.hpp>
@@ -50,6 +51,20 @@ TEMPLATE_TEST_CASE("Given some read-only grid",
     }
 }
 
+TEST_CASE("Given some grid which we fill", "[grid]")
+{
+    Grid<float> grid(64, 64, 8);
+    grid.fill(2.5);
+
+    SECTION("The values show up as the value we chose")
+    {
+        const size_t x = GENERATE(0, 1, 5, 14, 15);
+        const size_t y = GENERATE(0, 1, 5, 14, 15);
+        const size_t z = GENERATE(range(0, 8));
+        REQUIRE(grid.cell(x, y, z) == 2.5);
+    }
+}
+
 TEST_CASE("Given some two grids, initialized to zero", "[grid]")
 {
     Grid<float> A(64, 64, 8);
@@ -57,12 +72,63 @@ TEST_CASE("Given some two grids, initialized to zero", "[grid]")
     A.fill(0);
     B.fill(0);
 
-    SECTION("The values show up as zero")
+    SECTION("After running increment() on A -> B")
     {
-        const size_t x = GENERATE(0, 1, 5, 14, 15);
-        const size_t y = GENERATE(0, 1, 5, 14, 15);
-        const size_t z = GENERATE(range(0, 8));
-        REQUIRE(A.cell(x, y, z) == 0);
-        REQUIRE(B.cell(x, y, z) == 0);
+        pairwise(B, A, increment<float>());
+
+        SECTION("Each cell in B has been incremented once per neighbor")
+        {
+            const size_t x = GENERATE(0, 1, 5, 14, 15, 62, 63);
+            const size_t y = GENERATE(0, 1, 5, 14, 15, 62, 63);
+            const size_t z = GENERATE(range(0, 8));
+
+            const bool x_internal = (x > 0) && (x < 63);
+            const bool y_internal = (y > 0) && (y < 63);
+            const bool z_internal = (z > 0) && (z < 7);
+
+            const int num_axes_internal = int(x_internal) + int(y_internal) + int(z_internal);
+            const bool is_internal = num_axes_internal == 3;
+            const bool is_face = num_axes_internal == 2;
+            const bool is_edge = num_axes_internal == 1;
+            const bool is_corner = num_axes_internal == 0;
+
+            REQUIRE((is_internal || is_face || is_edge || is_corner));
+
+            SECTION("Internal cells have 6 neighbors")
+            {
+                if (is_internal)
+                {
+                    REQUIRE(B.cell(x, y, z) == 6.0);
+                }
+            }
+            SECTION("Cube face cells have 5 neighbors")
+            {
+                if (is_face)
+                {
+                    REQUIRE(B.cell(x, y, z) == 5.0);
+                }
+            }
+            SECTION("Cube edge cells have 4 neighbors")
+            {
+                if (is_edge)
+                {
+                    REQUIRE(B.cell(x, y, z) == 4.0);
+                }
+            }
+            SECTION("Cube corner cells have 3 neighbors")
+            {
+                if (is_corner)
+                {
+                    REQUIRE(B.cell(x, y, z) == 3.0);
+                }
+            }
+        }
+        SECTION("The values in A remain at zero")
+        {
+            const size_t x = GENERATE(0, 1, 5, 14, 15);
+            const size_t y = GENERATE(0, 1, 5, 14, 15);
+            const size_t z = GENERATE(range(0, 8));
+            REQUIRE(A.cell(x, y, z) == 0.0);
+        }
     }
 }
