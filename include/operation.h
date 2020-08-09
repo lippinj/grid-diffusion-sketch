2f0cc19323a940ab66238e958b46777cd0267116
Joonas Lipping <lippinj@outlook.com>
2020-08-09 16:40:18 +0300

    Add increment() test to simple_buffer.test

--- include/operation.h
+++ include/operation.h
@@ -1,3 +1,4 @@
 #include "operation/copy.h"
-#include "operation/fill.h"
 #include "operation/exchange_heat.h"
+#include "operation/fill.h"
+#include "operation/increment.h"
--- test/simple_buffer.test.cpp
+++ test/simple_buffer.test.cpp
@@ -1,4 +1,6 @@
 #include "buffer/simple_buffer.h"
+#include "operation.h"
+#include "traversal.h"
 
 #include <catch.hpp>
 
@@ -64,6 +66,77 @@ TEMPLATE_TEST_CASE(
                 for (size_t x = 0; x < 16; ++x)
                 {
                     REQUIRE(&buffer(z, y, x) == buffer(z, y) + x);
+                }
+            }
+        }
+    }
+}
+
+TEMPLATE_TEST_CASE("SimpleBuffer increment() produces correct results",
+                   "[buffer]",
+                   gd::buffer::SimpleBuffer<float>,
+                   gd::buffer::SimpleBuffer<int>,
+                   gd::buffer::m128::SimpleBuffer<float>,
+                   gd::buffer::m128::SimpleBuffer<int>)
+{
+    TestType A(64, 64, 8);
+    TestType B(64, 64, 8);
+    gd::fill<typename TestType::DataType>(A, 0);
+    gd::fill<typename TestType::DataType>(B, 0);
+
+    SECTION("After running increment() on A -> B")
+    {
+        gd::traversal::linearPairwise(B, A, gd::increment<typename TestType::DataType>());
+
+        SECTION("Each cell in B has been incremented once per neighbor")
+        {
+            for (size_t x : {0, 1, 5, 14, 15, 62, 63})
+            {
+                for (size_t y : {0, 1, 5, 14, 15, 62, 63})
+                {
+                    for (size_t z : {0, 1, 2, 3, 4, 5, 6, 7})
+                    {
+                        const bool x_internal = (x > 0) && (x < 63);
+                        const bool y_internal = (y > 0) && (y < 63);
+                        const bool z_internal = (z > 0) && (z < 7);
+
+                        const int num_axes_internal =
+                            int(x_internal) + int(y_internal) + int(z_internal);
+                        const bool is_internal = num_axes_internal == 3;
+                        const bool is_face = num_axes_internal == 2;
+                        const bool is_edge = num_axes_internal == 1;
+                        const bool is_corner = num_axes_internal == 0;
+
+                        if (is_internal)
+                        {
+                            REQUIRE(B(z, y, x) == 6);
+                        }
+                        if (is_face)
+                        {
+                            REQUIRE(B(z, y, x) == 5);
+                        }
+                        if (is_edge)
+                        {
+                            REQUIRE(B(z, y, x) == 4);
+                        }
+                        if (is_corner)
+                        {
+                            REQUIRE(B(z, y, x) == 3);
+                        }
+                    }
+                }
+            }
+        }
+        SECTION("The values in A remain at zero")
+        {
+            for (size_t x : {0, 1, 5, 14, 15})
+            {
+                for (size_t y : {0, 1, 5, 14, 15})
+                {
+                    for (size_t z : {0, 1, 2, 3, 4, 5, 6, 7})
+                    {
+                        REQUIRE(A(z, y, x) == 0);
+                    }
                 }
             }
         }
