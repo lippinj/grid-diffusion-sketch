f42e028db16803d22141dbfa38c8125e3dd5a9c7
Joonas Lipping <lippinj@outlook.com>
2020-08-09 17:34:16 +0300

    Add tests for m128::copy()

--- test/copy.test.cpp
+++ test/copy.test.cpp
@@ -1,15 +1,18 @@
 #include "buffer/simple_buffer.h"
+#include "operation/copy.h"
 #include "operation/fill.h"
 
 #include <catch.hpp>
 
-TEMPLATE_TEST_CASE("gd::fill() is correct on SimpleBuffer<float,*>",
+TEMPLATE_TEST_CASE("gd::copy() is correct on SimpleBuffer<float,*>",
                    "[buffer]",
                    gd::buffer::SimpleBuffer<float>,
                    gd::buffer::m128::SimpleBuffer<float>)
 {
-    TestType buffer(256, 256, 8);
-    gd::fill(buffer, 3.5f);
+    TestType src(256, 256, 8);
+    TestType dst(256, 256, 8);
+    gd::fill(src, 3.5f);
+    gd::copy(dst, src);
 
     for (size_t z = 0; z < 8; ++z)
     {
@@ -17,18 +20,20 @@ TEMPLATE_TEST_CASE("gd::fill() is correct on SimpleBuffer<float,*>",
         {
             for (size_t x = 0; x < 256; ++x)
             {
-                REQUIRE(buffer(z, y, x) == 3.5f);
+                REQUIRE(dst(z, y, x) == 3.5f);
             }
         }
     }
 }
 
-TEMPLATE_TEST_CASE("gd::m128::fill() is correct on SimpleBuffer<float,*>",
+TEMPLATE_TEST_CASE("gd::m128::copy() is correct on SimpleBuffer<float,*>",
                    "[buffer]",
                    gd::buffer::m128::SimpleBuffer<float>)
 {
-    TestType buffer(256, 256, 8);
-    gd::m128::fill(buffer, 3.5f);
+    TestType src(256, 256, 8);
+    TestType dst(256, 256, 8);
+    gd::m128::fill(src, 3.5f);
+    gd::m128::copy(dst, src);
 
     for (size_t z = 0; z < 8; ++z)
     {
@@ -36,7 +41,7 @@ TEMPLATE_TEST_CASE("gd::m128::fill() is correct on SimpleBuffer<float,*>",
         {
             for (size_t x = 0; x < 256; ++x)
             {
-                REQUIRE(buffer(z, y, x) == 3.5f);
+                REQUIRE(dst(z, y, x) == 3.5f);
             }
         }
     }
